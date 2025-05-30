cddf38cf0c51a55931cbef671c74a1f43d399159
Hans Kyei <baffourhans@gmail.com>
2025-05-30 21:03:15 +0000

    updated matchCaseGame

--- matchCaseGame.py
+++ matchCaseGame.py
@@ -1,10 +1,20 @@
 import random
-secret_number = random.randint(1,10) #Generate random numbers between 1 and 10
-guess = int(input("Guess number between 1 and 10: "))
-match guess:
-    case _ if guess == secret_number:
-        print("Congratulations, you guessed it!", secret_number)
-    case _  if guess > secret_number:
-        print("Oops, your guess is a bit high")
-    case _ if guess < secret_number:
-        print("Nope, your guess is a bit low")
+counter = 0
+while True:
+    secret_number = random.randint(1,10) #Generate random numbers between 1 and 10
+    guess = int(input("Guess number between 1 and 10: "))
+    counter += 1
+    match guess:
+        case _ if guess == secret_number:
+            print("Congratulations, you guessed it!")
+            print (f"It took you {counter} guesses to get it right")
+        case _  if guess > secret_number:
+            print("Oops, your guess is a bit high")
+        case _ if guess < secret_number:
+            print("Nope, your guess is a bit low")
+    play_again = input("Do you want to play again?(yes/no)")
+    if play_again != "yes":
+        print("Thanks for playing! Goodbye 👋")
+        
+        break
+    
