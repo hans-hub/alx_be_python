b0e6c1ea2f042f2cf12d2b6b87561b7c14f25bed
Hans Kyei <baffourhans@gmail.com>
2025-06-08 23:08:35 +0000

    updated temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,23 +1,21 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
-#Function to convert from Fahrenheit to celsius
-
+# Function to convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
-    return FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
-
+# Function to convert Celsius to Fahrenheit
 def convert_to_fahrenheit(celsius):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
-    return CELSIUS_TO_FAHRENHEIT_FACTOR * (celsius + 32)
- 
+    return CELSIUS_TO_FAHRENHEIT_FACTOR * celsius + 32
 
-temprature = float(input("Enter the temprature to convert: "))
-option = input("Is this temperature Celsius or Fahrenheit? (C/F): ")
+# Main Program
+temperature = float(input("Enter the temperature to convert: "))
+option = input("Is this temperature Celsius or Fahrenheit? (C/F): ").strip().upper()
+
 if option == "C":
-    print(f"{temprature}℃ is {convert_to_fahrenheit(temprature)}℉")
+    print(f"{temperature}℃ is {convert_to_fahrenheit(temperature):.2f}℉")
 elif option == "F":
-    print(f"{temprature}℉ is {convert_to_celsius(temprature)}℃")
+    print(f"{temperature}℉ is {convert_to_celsius(temperature):.2f}℃")
 else:
-    print("choose the right option")
+    print("Please choose the correct option (C or F).")
